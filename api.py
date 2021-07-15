23fe1c476b1942e130b8d9ff85620d53719d03e6
5CITe <arch.hack28@gmail.com>
2021-07-15 16:51:32 +0530

    stats function done

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import json
 import aiohttp
 import asyncio
+import webbrowser
 
 async def cred_validate(username,tag):
     async with aiohttp.ClientSession() as session:
@@ -13,4 +14,48 @@ async def live_match(username,tag):
         async with session.get(f'https://api.henrikdev.xyz/valorant/v1/live-match/{username}/{tag}') as r:
             data=await r.json()
             return data
-        
+
+async def mmr(username,tag,region):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://api.henrikdev.xyz/valorant/v2/mmr/{region}/{username}/{tag}') as r:
+            data = (await r.json())["data"]
+            return data['current_data']
+
+
+async def statistics(username,tag):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.tracker.gg/api/v2/valorant/standard/profile/riot/{username}%23{tag}", json={}) as r:
+            data = await r.json()
+            try:
+                if data['errors'][0]['message']=='This profile is still private.':
+                    webbrowser.open(f'https://tracker.gg/valorant/profile/riot/{username}%23{tag}/overview')
+            except KeyError:
+                user = data['data']["platformInfo"]["platformUserHandle"]
+                avatarUrl = data['data']["platformInfo"]["avatarUrl"]
+
+                stats = data['data']["segments"][0]["stats"]
+                win_pct = stats["matchesWinPct"]["displayValue"]
+                hs_pct = stats["headshotsPercentage"]["displayValue"]
+                kd_ratio = stats["kDRatio"]["displayValue"]
+                aces = stats["aces"]["displayValue"]
+                time_played = stats["timePlayed"]["displayValue"]
+                rank = stats["rank"]["metadata"]["tierName"]
+                rankIconUrl = stats["rank"]["metadata"]["iconUrl"]
+
+                DATA = dict(
+                    user=user,
+                    avatarUrl=avatarUrl,
+                    win_pct=win_pct,
+                    hs_pct=hs_pct,
+                    kd_ratio=kd_ratio,
+                    aces=aces,
+                    time_played=time_played,
+                    rank=rank,
+                    rankIconUrl=rankIconUrl,
+                )
+                return DATA
+
+                
+
+    
+
--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import aiohttp
 from api import *
 import re
 import datetime
+from util import *
 
 
 token = open("token.txt","r").readline()
@@ -21,12 +22,12 @@ async def on_command_error(ctx,error):
         #await ctx.message.delete()
 
 @bot.command()
-async def link(ctx,username,tag):
-    tag=re.sub("^#",'',tag)
+async def link(ctx,username,itag):
+    tag=re.sub("^#",'',itag)
     data= await cred_validate(username,tag)
     if data['status']=='200':
         db.set_record(ctx.message.author.id,username,tag,data['data'])
-        await ctx.send(f"{ctx.message.author.mention} you are linked :white_check_mark:")
+        await ctx.send(f"{ctx.message.author.mention} you are linked ☑")
     elif data['status']=='404':
         await ctx.send(data['message'])
     else:
@@ -37,14 +38,17 @@ async def link(ctx,username,tag):
 
 @bot.command()
 async def rank(ctx, member: discord.Member=None):
-    if member is None:
-        member = ctx.message.author
-    await ctx.send(f"{member.mention} hello sir")
+    if member is not None:
+        await ctx.send(f"{member.mention} hello")
+    else:
+        await ctx.send(ctx.message.author.mention)
+    
 
+''' 
 @bot.command()
 async def status(ctx, region):
     if region in ('eu', 'ap', 'na', 'kr'):
-        await ctx.send
+'''
 
 @bot.command()
 async def live(ctx,member:discord.Member):
@@ -79,22 +83,58 @@ async def live(ctx,member:discord.Member):
             else:
                 score=f"[{data2['score_ally_team']}:{data2['score_enemy_team']}]"
                 if data2["custom_game"]=="true":
-                    sym="::white_check_mark:"
+                    sym="✅"
                 else:
-                    sym=":x:"
+                    sym="❌"
                 
                 embed=discord.Embed(title=data2['current_state'], description=f"custom {sym}")
+                embed.set_thumbnail(url=gamemode_icons[gamemode])
                 embed.set_author(name="{} #{}".format(details['display_name'],details['tag'])) 
                 embed.add_field(name="game mode", value=f"{gamemode} {score}", inline=True)
                 embed.add_field(name='\b', value='\b',inline=True)
                 embed.add_field(name="MAP", value=data2['map'], inline=True)
-                embed.timestamp = datetime.datetime.utcnow()
-                embed.set_footer(text='\u200b')
+                #embed.timestamp = datetime.datetime.utcnow()
+                embed.set_footer(url="https://https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/listviewicon.png")
                 await ctx.send(embed=embed)
-                
-                
+                         
         else:
             await ctx.send("SERVER ERROR")
+
+@bot.command()
+async def stats(ctx,member:discord.Member=None):
+
+    if member is None:
+        member=ctx.message.author
+    details=db.get_record(member.id)
+    if details is None:
+        await ctx.send(f'{member.mention} has not linked their account')
+    else:
+        p1=await statistics(details['display_name'],details['tag'])
+        p1['acc_level']=details['details']['account_level']
+        p1mmr=await mmr(details['display_name'],details['tag'],details['details']['region'])
+
+        if p1mmr['games_needed_for_rating']!=0:
+            rank="unranked"
+            description=""
+        else:
+            rank=p1mmr['currenttierpatched']
+            description =  f"**{p1mmr['ranking_in_tier']}/100** RR | **{p1mmr['elo']}** ELO"
+        embed = discord.Embed(title=rank, description=description)
+        embed.set_thumbnail(url=p1["rankIconUrl"])
+        embed.set_author(name=p1['user'], icon_url=p1["avatarUrl"])
+        embed.add_field(name="K/D Ratio", value=p1["kd_ratio"])
+        embed.add_field(name="\u200b", value="\u200b")
+        embed.add_field(name="Aces", value=p1["aces"])
+        
+        embed.add_field(name="Headshot %", value=p1["hs_pct"])
+        embed.add_field(name="\uFEFF", value="\uFEFF")
+        embed.add_field(name="Win %", value=p1["win_pct"])
+        embed.add_field(name="Time Played", value=p1["time_played"])
+        embed.add_field(name="\u200b", value="\u200b")
+        embed.add_field(name="Account Level",value=p1['acc_level'])
+        await ctx.send(embed=embed)
+ 
+    
 
 bot.run(token)
 
